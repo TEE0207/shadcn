db3c137040684d61407b7e95081cc9779adc2adb
Taofeek Lawal <taofeeklwl@gmail.com>
2025-06-18 20:03:31 +0100

    side bar wip

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react"
+"use client"
+
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react"
 import Link from "next/link"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import {
@@ -10,8 +12,11 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "./ui/button"
+import { useTheme } from "next-themes"
 
 const Navbar = () => {
+
+    const {theme, setTheme} = useTheme()
 
     return(
         <nav className="p-4 flex items-center justify-between"> 
@@ -24,9 +29,30 @@ const Navbar = () => {
             <div className="flex items-center gap-4">
 
                 <Link href ="/"> Dashboard</Link>
-                <Moon />
+                {/* THEME MENU */}
 
-               
+                 <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                        <span className="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                    </DropdownMenu>
+
+                {/* USER MENU */}
                 <DropdownMenu>
                     <DropdownMenuTrigger>
 
@@ -54,26 +80,7 @@ const Navbar = () => {
                     </DropdownMenuContent>
                 </DropdownMenu>
 
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant = "outline" size = "icon" >
-              <SquareMenu />
-              
-              {/* sr-only for screen readers */}
-              <span className="sr-only">Open Menu</span>
-              </Button>
-
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-
+          
             </div>
         </nav>
     )
