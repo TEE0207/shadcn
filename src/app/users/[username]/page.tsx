20175ef1992d72ffe699c77b377be6f474d045c7
Taofeek Lawal <taofeeklwl@gmail.com>
2025-06-21 17:30:36 +0100

    badge updated

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -1,12 +1,131 @@
+import CardList from '@/components/CardList'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
+import { BadgeCheck, Candy, Citrus, Shield } from 'lucide-react'
 import React from 'react'
 
 const SingleUserPage = () => {
   return (
 
     <div>
-        last
+     <Breadcrumb>
+         <BreadcrumbList>
 
-    </div>
+                <BreadcrumbItem>
+                <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+                </BreadcrumbItem>
+
+      <BreadcrumbSeparator />
+
+                <BreadcrumbItem>
+                <BreadcrumbLink href="/users">Users</BreadcrumbLink>
+                </BreadcrumbItem>
+
+      <BreadcrumbSeparator />
+
+            <BreadcrumbItem>
+            <BreadcrumbPage>Lawal Taofeek</BreadcrumbPage>
+            </BreadcrumbItem>
+
+    </BreadcrumbList>
+
+</Breadcrumb>
+
+{/* CONTAINER */}
+<div className='mt-4 flex flex-col xl:flex-row gap-8'>
+        {/* LEFT */}
+        <div className='w-full xl:w-1/3 space-y-6'>  
+
+                {/* USER BADGES CONTAINER */}
+                <div className='bg-primary-foreground p-4 rounded-lg'> 
+                    <h1 className='text-xl font-semibold'>User Badges</h1>
+                   <div className='flex gap-4 mt-4'>
+
+                       <HoverCard>
+                            <HoverCardTrigger>
+                                <BadgeCheck size={36} className='rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2'/>
+                            </HoverCardTrigger>
+
+                             <HoverCardContent>
+                                <h1 className='font-bold mb-2'>Verified User</h1>
+                                <p className='text-sm text-muted-foreground'>This user has been verified by the admin.</p>
+                             </HoverCardContent>
+                       </HoverCard>
+
+                       <HoverCard>
+                            <HoverCardTrigger>
+                                <Citrus size={36} className='rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2'/>
+                            </HoverCardTrigger>
+
+                             <HoverCardContent>
+                                <h1 className='font-bold mb-2'>Popular</h1>
+                                <p className='text-sm text-muted-foreground'>This user has been popular in the community.</p>
+                             </HoverCardContent>
+                       </HoverCard>
+
+                       <HoverCard>
+                            <HoverCardTrigger>
+                                <Shield size={36} className='rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2'/>
+                            </HoverCardTrigger>
+
+                             <HoverCardContent>
+                                <h1 className='font-bold mb-2'>Admin</h1>
+                                <p className='text-sm text-muted-foreground'>This user have access to all features and can manage.</p>
+                             </HoverCardContent>
+                       </HoverCard>
+
+                       <HoverCard>
+                            <HoverCardTrigger>
+                                <Candy size={36} className='rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2'/>
+                            </HoverCardTrigger>
+
+                             <HoverCardContent>
+                                <h1 className='font-bold mb-2'>Awarded</h1>
+                                <p className='text-sm text-muted-foreground'>This user has been awarded for their contributions.</p>
+                             </HoverCardContent>
+                       </HoverCard>
+                   </div>
+
+                </div>
+
+
+                                  {/* INFOMATION CONTAINER */}
+                <div className='bg-primary-foreground p-4 rounded-lg'>
+                     <h1 className='text-xl font-semibold'>User Information</h1>
+                     <div className='space-y-4 mt-4'>
+                        <div className='flex flex-col gap-2 mb-8'>
+                            <p className='text-sm text-muted-foreground'>Profile completion</p>
+
+                        </div>
+
+                     </div>
+
+                </div>
+
+                                 {/* CARDLIST CONTAINER */}
+                <div className='bg-primary-foreground p-4 rounded-lg'>
+                    <CardList title='Recent Transactions' />
+                </div>
+
+        
+         </div>
+
+
+          {/* RIGHT */}
+        <div className='w-full xl:w-2/3 space-y-6'> 
+
+              {/* USER CARD CONTAINER */}
+           <div className='bg-primary-foreground p-4 rounded-lg'>User Card</div>
+           
+             {/* CHART CONTAINER */}
+           <div className='bg-primary-foreground p-4 rounded-lg'>Chart</div>
+
+        
+         </div>
+
+</div>
+
+</div>
   )
 }
 
